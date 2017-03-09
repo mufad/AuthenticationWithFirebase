60db35254414dddb9d52e8bb13ee0072de91cfdf
Monwar Jahan <mmufad@gmail.com>
2017-03-10 00:31:17 +0600

    Initial commit

---
+++ app/src/main/java/com/droiddigger/authenticationwithfirebase/MainActivity.java
@@ -0,0 +1,22 @@
+package com.droiddigger.authenticationwithfirebase;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        
+    }
+}
